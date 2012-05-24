6f4451de55a91c785ea3d1db23c3ddb53f9801fc
Andy Gordon <andy@kujilabs.com>
2012-05-24 18:51:51 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeninsularBay::Application.config.secret_token = '740bf8e3c27167275530dfa4e8508e8c780b92c20a4cbc0bdcea68506460b74942334bb6d59718e0938db769cf23f882caadd27793e2b30d5d33fd4cf4860b21'
